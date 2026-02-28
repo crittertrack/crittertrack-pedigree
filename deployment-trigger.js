41fa8913f56d7b6378631de582dad5f61ab0ac33
Crittertrack <crittertrackowner@gmail.com>
2026-02-28 12:01:50 +0100

    Trigger deployment for mate data and email endpoint testing

---
+++ deployment-trigger.js
@@ -0,0 +1,7 @@
+// Force deployment trigger - mate data testing
+// Date: 2026-02-28
+// Purpose: Trigger Railway deployment to test mate data persistence and email endpoint
+module.exports = {
+    DEPLOYMENT_TIMESTAMP: '2026-02-28T11:55:00Z',
+    FEATURES: ['mate_data_debug', 'email_endpoint_fix']
+};
