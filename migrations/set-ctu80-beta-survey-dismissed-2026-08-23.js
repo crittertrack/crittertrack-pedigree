e8f4015e8c3eed6786dc45a787463150f285ba22
Crittertrack <crittertrackowner@gmail.com>
2026-08-23 12:56:56 +0200

    xx

---
+++ migrations/set-ctu80-beta-survey-dismissed-2026-08-23.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { PublicProfile } = require('../database/models');
+
+(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    const profile = await PublicProfile.findOne({ id_public: 'CTU80' });
+    if (!profile) {
+        console.log('CTU80 profile not found');
+        await mongoose.disconnect();
+        return;
+    }
+
+    console.log('Before:', profile.betaSurveyStatus, profile.betaSurveyLastPromptedAt);
+    profile.betaSurveyStatus = 'dismissed';
+    await profile.save();
+
+    const verify = await PublicProfile.findOne({ id_public: 'CTU80' });
+    console.log('After:', verify.betaSurveyStatus, verify.betaSurveyLastPromptedAt);
+
+    await mongoose.disconnect();
+})();
